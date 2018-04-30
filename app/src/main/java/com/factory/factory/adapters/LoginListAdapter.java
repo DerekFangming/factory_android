f4dc6ad530e1259d04ac2096525562f5ecbd30c0
synfm@126.com <synfm@126.com>
2018-04-29 23:06:24 -0400

    Adding login page UI elements

--- app/src/main/java/com/factory/factory/adapters/LoginListAdapter.java
+++ app/src/main/java/com/factory/factory/adapters/LoginListAdapter.java
@@ -1,5 +1,6 @@
 package com.factory.factory.adapters;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,26 +16,46 @@ public class LoginListAdapter extends RecyclerView.Adapter<ViewHolder>  {
 
     @Override
     public int getItemViewType(int position) {
-        return 1;
+        if (position == 0) {
+            return 1;
+        } else if (position < 3) {
+            return 2;
+        } else if (position == 3) {
+            return 3;
+        } else {
+            return 4;
+        }
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    @NonNull
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         switch (viewType) {
             case 1:
                 View view1 = LayoutInflater.from(parent.getContext()).inflate(R.layout.cell_login_logo, parent, false);
                 return new ViewHolder(view1);
+            case 2:
+                View view2 = LayoutInflater.from(parent.getContext()).inflate(R.layout.cell_login_textview, parent, false);
+                return new ViewHolder(view2);
+            case 3:
+                View view3 = LayoutInflater.from(parent.getContext()).inflate(R.layout.cell_login_button, parent, false);
+                return new ViewHolder(view3);
+            case 4:
+                View view4 = LayoutInflater.from(parent.getContext()).inflate(R.layout.cell_login_options, parent, false);
+                return new ViewHolder(view4);
+            default:
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cell_empty, parent, false);
+                return new ViewHolder(view);
         }
-        return null;
     }
 
     @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
-        View cell = holder.itemView;
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
+        //View cell = holder.itemView;
     }
 
     @Override
     public int getItemCount() {
-        return 1;
+        return 5;
     }
 }
